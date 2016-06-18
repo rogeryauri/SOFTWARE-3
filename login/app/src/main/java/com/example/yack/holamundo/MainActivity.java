e9fd54f37fa9308c5e34b5d2d484307f87e8686b
roger <rogeryauri@gmail.com>
2016-06-18 21:42:02 +0200

    creacion de login

---
+++ login/app/src/main/java/com/example/yack/holamundo/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.yack.holamundo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextInputLayout til_email;
+    TextInputLayout til_pass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        til_email = (TextInputLayout) findViewById(R.id.til_email);
+        til_pass = (TextInputLayout) findViewById(R.id.til_pass);
+    }
+    public void cickValidad(View view){
+    String email = til_email.getEditText().getText().toString().trim();
+    String pass = til_pass.getEditText().getText().toString().trim();
+        if(TextUtils.isEmpty(email)){
+            til_email.setError("Email incorrecto");
+            return;
+        }
+        if(TextUtils.isEmpty(pass)){
+            til_pass.setError("Contraseña incorrecto");
+            return;
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
