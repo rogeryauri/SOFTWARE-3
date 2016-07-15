ce424528a43b315f3b4232bc5c48cc4e25f2a177
roger <rogeryauri@gmail.com>
2016-07-15 23:12:52 +0200

    conexion/registro de usuario con mysql

--- login/app/src/main/java/com/example/yack/holamundo/MainActivity.java
+++ login/app/src/main/java/com/example/yack/holamundo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.yack.holamundo;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,6 +8,7 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,6 +17,15 @@ import android.widget.EditText;
 import android.os.Bundle;
 import android.content.Intent;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -26,8 +37,8 @@ public class MainActivity extends AppCompatActivity {
         final EditText txtpass = (EditText) findViewById(R.id.txtpass);
         final Button txtboton = (Button) findViewById(R.id.txtboton);
         final TextView registerLink  = (TextView) findViewById(R.id.textregistro);
-
-    registerLink.setOnClickListener(new View.OnClickListener() {
+        
+        registerLink.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent registerIntent =new Intent(MainActivity.this, registro.class);
@@ -35,9 +46,5 @@ public class MainActivity extends AppCompatActivity {
         }
     });
     }
-
-
-
-
 
 }
--- login/app/src/main/java/com/example/yack/holamundo/registro.java
+++ login/app/src/main/java/com/example/yack/holamundo/registro.java
@@ -1,13 +1,34 @@
 package com.example.yack.holamundo;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class registro extends AppCompatActivity {
 
@@ -17,13 +38,101 @@ public class registro extends AppCompatActivity {
         setContentView(R.layout.activity_registro);
 
         final EditText textnom1 = (EditText) findViewById(R.id.textnom1);
-        final EditText textapell = (EditText) findViewById(R.id.textapell);
+        final EditText textcorreo = (EditText) findViewById(R.id.textcorreo);
         final EditText textpass1 = (EditText) findViewById(R.id.textpass1);
-        final EditText textpass2 = (EditText) findViewById(R.id.textpass2);
+        final EditText textuser = (EditText) findViewById(R.id.textuser);
         final EditText textdni = (EditText) findViewById(R.id.textdni);
         final Button btnregistro = (Button) findViewById(R.id.btnregistro);
 
 
+
+        btnregistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new CargarDatos().execute("http://10.0.2.2:8080/login/registro.php?nombres="+textnom1.getText().toString()+"&email="+textcorreo.getText().toString()+"&contrasenia="+textpass1.getText().toString()+"&usuario="+textuser.getText().toString()+"&dni="+textdni.getText().toString());
+
+            }
+        });
+
+
+    }
+    private class CargarDatos extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+
+            Toast.makeText(getApplicationContext(), "Se almacenaron correctamente", Toast.LENGTH_LONG).show();
+
+        }
     }
 
+    private class ConsultarDatos extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+
+                   }
+    }
+
+    private String downloadUrl(String myurl) throws IOException {
+        Log.i("URL",""+myurl);
+        myurl = myurl.replace(" ","%20");
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("respuesta", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
 }
