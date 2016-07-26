bcfa01f42a6abf4cf1a16d708764f62c7f712260
roger <rogeryauri@gmail.com>
2016-07-26 12:29:38 +0200

    botones que permite llamar a otros activitys

--- login/app/src/main/java/com/example/yack/holamundo/Principales_lugares.java
+++ login/app/src/main/java/com/example/yack/holamundo/Principales_lugares.java
@@ -1,29 +1,30 @@
 package com.example.yack.holamundo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Principales_lugares extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_principales_lugares);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_ponceca_catalogo);
+        final ImageButton registerLink  = (ImageButton) findViewById(R.id.imgponceca);
+        registerLink.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent registerIntent =new Intent(Principales_lugares.this, Ponceca_catalogo.class);
+                Principales_lugares.this.startActivity(registerIntent);
             }
         });
+
     }
 
 }
